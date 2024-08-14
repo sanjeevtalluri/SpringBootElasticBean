6965a14c4ce9e99d2449198e2a6728cf9abab793
sanjeevtalluri <sanjeevtalluri@gmail.com>
2024-08-14 15:50:58 +0530

    first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,16 @@
+/*https://www.browserling.com/tools/bcrypt Use Rounds 10*/
+
+/*in28minutes/dummy
+INSERT INTO user (id, username, password, role) VALUES (1, 'in28minutes', '$2a$10$3zHzb.Npv1hfZbLEU5qsdOju/tk2je6W6PnNnY.c1ujWPcZh4PL6e','ROLE_USER');
+
+/*in28minutes2/mypassword*/
+INSERT INTO user (id, username, password, role) VALUES (2, 'in28minutes2', '$2a$10$i9AckmxMkb4yKtLCdxeQheCm2pXWB3qZ2G189/Ph/DUci1DvLO.Rq','ROLE_USER');
+
+INSERT INTO todo(id, username,description,target_date,is_done)
+VALUES(10001, 'in28minutes', 'Learn JPA', sysdate(), false);
+
+INSERT INTO todo(id, username,description,target_date,is_done)
+VALUES(10002, 'in28minutes', 'Learn Data JPA', sysdate(), false);
+
+INSERT INTO todo(id, username,description,target_date,is_done)
+VALUES(10003, 'in28minutes', 'Learn Microservices', sysdate(), false); */
